f203c8345cd7b770e3d483c509a4f4adbde6ef30
WwwanQin <xm36447864@126.com>
2024-08-12 00:38:00 +0800

    feat: "fix packaging error"

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: zouwenqin
+ * @Date: 2024-07-29 23:50:31
+ * @LastEditors: zouwenqin
+ * @LastEditTime: 2024-08-12 00:36:36
+ */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+};
 
 export default nextConfig;
--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -4,7 +4,7 @@
  * @Author: zouwenqin
  * @Date: 2024-08-11 13:49:30
  * @LastEditors: zouwenqin
- * @LastEditTime: 2024-08-12 00:26:28
+ * @LastEditTime: 2024-08-12 00:34:25
  */
 "use client";
 import styles from "./page.module.scss";
@@ -28,10 +28,17 @@ const Dashboard = () => {
       <Grid container spacing={2}>
         {list.map((ele: ILessonInfo, index: number) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
-            <CourseCard info={ele} handleClick={(info: ILessonInfo) => {
-              let pageList = ['/lesson/firstLesson','/lesson/secondLesson','/lesson/thirdLesson'];
-              router.push(`${pageList[index]}`);
-            }}/>
+            <CourseCard
+              info={ele}
+              handleClick={(info: ILessonInfo) => {
+                let pageList = [
+                  "/lesson/firstLesson",
+                  "/lesson/secondLesson",
+                  "/lesson/thirdLesson",
+                ];
+                router.push(`${pageList[index]}`);
+              }}
+            />
           </Grid>
         ))}
       </Grid>
